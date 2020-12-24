0f9da982c2134ffdc398868d6eaadc4291c93b69
Zhenyang Hua <edison.hua@gmail.com>
2020-12-24 12:29:13 -0500

    Added snow controls

--- src/components/snow-intro.js
+++ src/components/snow-intro.js
@@ -1,28 +1,56 @@
 import {LitElement, css, html} from "lit-element";
 import Snow from 'effect-snow';
+import {button} from "../styles";
+
+const codeText = `
+// Create a snow scene instance and 
+// mount it to the container element
+const snow = new Snow(containerElement);
+// To start
+snow.start();
+// To stop 
+snow.stop();
+`;
 
 class SnowIntro extends LitElement {
   static get styles() {
-    return css`
-      #snowbox {
-        display: flex;
-        flex-wrap: wrap;
-        background-color: rgb(1 54 64);
-      }
+    return [
+      button,
+      css`
+        #snowbox {
+          display: flex;
+          flex-wrap: wrap;
+          background-color: rgb(1 54 64);
+        }
 
-      #snowbox > * {
-        flex: 1 1 auto;
-      }
-      
-      pre {
-        margin: 0 !important;
-        background-color: rgb(1 54 64) !important;
-      }
-      
-      * {
-        color: white;
-      }
-    `;
+        #snowbox > * {
+          flex: 1 1 auto;
+        }
+
+        pre {
+          margin: 0 !important;
+          background-color: rgb(1 54 64) !important;
+          font-size: 1.2rem !important;
+        }
+        
+        p {
+          font-size: 1.2rem;
+        }
+        
+        a {
+          background-color: white;
+          color: rgb(1 54 64);
+          border-radius: 4px;
+          padding: 0 4px;
+          font-size: 1.1rem;
+          font-family: "Roboto", Arial, SansSerif, sans-serif !important;
+        }
+
+        * {
+          color: white;
+        }
+      `
+    ];
   }
 
   render() {
@@ -32,15 +60,14 @@ class SnowIntro extends LitElement {
         <div>
           <h1>Like this effect?</h1>
           <p>
-            Try the <a target="_blank" href="https://github.com/zhenyanghua/effects">snow effect</a> in your site with three lines of code
+            Try the <a target="_blank" href="https://github.com/zhenyanghua/effects">effect-snow</a> in your site with
+            three lines of code
           </p>
+          <button class="inverted" @click="${this._startSnow}">start the snow</button>
+          <button class="inverted" @click="${this._stopSnow}">stop the snow</button>
         </div>
         <pre>
-          <code class="lang-js">
-const snow = new Snow(element);
-snow.start();
-snow.stop();
-          </code>
+          <code class="lang-js">${codeText}</code>
         </pre>
       </div>
     `;
@@ -54,8 +81,23 @@ snow.stop();
 
     // snow fall
     const container = this.shadowRoot.querySelector('#snowbox');
-    const snow = new Snow(container, { intensity: 2 });
-    snow.start();
+    this._snow = new Snow(container, {intensity: 2});
+    this._startSnow();
+  }
+
+  _startSnow() {
+    this._snow.start();
+    if (this._nextTimeToStopSnow) {
+      clearTimeout(this._nextTimeToStopSnow);
+    }
+    this._nextTimeToStopSnow = setTimeout(() => {
+      this._stopSnow();
+      this._nextTimeToStopSnow = null;
+    }, 120000);
+  }
+
+  _stopSnow() {
+    this._snow.stop();
   }
 }
 
--- src/styles.js
+++ src/styles.js
@@ -10,3 +10,19 @@ export const hidden = css`
     overflow: hidden;
   }
 `;
+
+export const button = css`
+  button {
+    color: white;
+    background-color: rgb(1 54 64);
+    border-color: white;
+    font-size: 1.2rem;
+    border-radius: 4px;
+  }
+  
+  button.inverted {
+    color: rgb(1 54 64);
+    background-color: white;
+    border-color: rgb(1 54 64);
+  }
+`;
