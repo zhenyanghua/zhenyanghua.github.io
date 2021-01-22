b2bc9a596f8667b31b3fb075e90315580aeb2c8e
Zhenyang Hua <edison.hua@gmail.com>
2021-01-22 15:03:06 -0500

    Updates

---
+++ chunks/writings.646939ef.js
@@ -0,0 +1,1 @@
+import{m as t,f as i}from"../index.9aae7637.js";import"./time.daaab1ba.js";import{W as r}from"./index.7fca6634.js";export default function(){return t`<${r} posts=${i} page=${{current:4,total:5,prev:"/writings/3",next:"/writings/5",last:"/writings/5",first:"/writings/1"}}/>`}
