e936e05f428bd4833ceb3502224a6286b4a3dbc2
Zhenyang Hua <edison.hua@gmail.com>
2021-09-22 10:58:37 -0400

    Updates

---
+++ chunks/index.6621ce0a.js
@@ -0,0 +1,30 @@
+import{s as e,m as s,b as a,y as l,u as t,e as n}from"../index.97203252.js";import{f as r}from"./time.daaab1ba.js";e("/assets/style.module.2e651384.css");const c="writings_j5bi2k",i="list_j5bi2k",u="card_j5bi2k";null;const $="card_cwjn7n",o="title_cwjn7n",p="summary_cwjn7n",d="date_cwjn7n",f="readmore_cwjn7n";function v({id:e,class:a,url:l,title:t,summary:n,date:c}){let i=$;return a&&(i=`${i} ${a}`),s`<div id=${e} class=${i}>
+      <h2 class=${o}>
+        <a href=${l}>${t}</a>
+      </h2>
+      <p class=${d}>${r(c)}</p>
+      <p class=${p} dangerouslySetInnerHTML=${{__html:n}}/>
+      <p class=${f} aria-label=${"Read more about "+t}>
+        <a href=${l}>Read more</a>
+      </p>
+    </div>`}null;const m="page_1es3dyz";function h({prev:e,current:a,first:l,last:t,total:n,next:r}){return s`<div class=${m}>
+      <span><a href=${l}>1</a></span>
+      <span><a href=${e}>Prev</a></span>
+      <span>${a} / <a href=${t}>${n}</a></span>
+      <span><a href=${r}>Next</a></span>
+    </div>`}null;const _="search_1pt434p",b="searchBox_1pt434p",w="hidden_1pt434p",y="active_1pt434p";function g({posts:e}){const[t,n]=a(""),[r,c]=a(null),[i,u]=a(null),$=e.filter(e=>e.title.toLowerCase().includes(t.trim().toLowerCase())).slice(0,5),o=e=>{location.assign(e)};return l(()=>{null!==i&&o($[i].url)},[i]),s`<div class=${_} onKeyDown=${e=>{let s=r;switch(e.key){case"ArrowDown":e.preventDefault(),s=null===s?0:(s+1)%$.length;break;case"ArrowUp":e.preventDefault(),s=null===s?$.length-1:(s-1+$.length)%$.length;break;case"Enter":e.preventDefault(),u(s);break;case"Escape":e.preventDefault(),s=null}c(s)}} onBlur=${e=>{e.preventDefault(),c(null)}}>
+            <div class=${b}>
+                <label for="search">Search Article</label>
+                <input id="search" type="text" value=${t} onInput=${e=>{n(e.target.value),c(0)}}/>
+            </div>
+            <ul class=${null!==r&&$[r]?"":w}>
+                ${$.map(e=>s`<li key=${e.url} class=${null!==r&&$[r]&&e.url===$[r].url?y:""} onClick=${()=>{console.debug("clicked"),o(e.url)}}>${e.title}</li>`)}
+            </ul>
+        </div>`}function k({posts:e,page:a}){return t("Writings"),s`<div class=${c}>
+      <${g} posts=${n}/>
+      <${h} ...${a}/>
+      <div class=${i}>
+        ${e.map(e=>s`<${v} id=${e.url} class=${u} key=${e.url} ...${e} url=${e.url+"#maincontent"}/>`)}
+      </div>
+      <${h} ...${a}/>
+    </div>`}export{k as W};
---
+++ chunks/index.c7014547.js
@@ -0,0 +1,9 @@
+import{s,u as a,c as t,n as e,m as d}from"../index.950db7b4.js";import{f as r}from"./time.daaab1ba.js";s("/assets/style.module.184703f0.css");const n="post_14b9dxr",o="title_14b9dxr",i="date_14b9dxr",b="summary_14b9dxr",h="github_14b9dxr";function l({children:s,title:l,date:m,summary:u}){a(l),t();const{path:c}=e();return d`<div class=${n}>
+      <h1 class=${o}>${l}</h1>
+      <p class=${i}>Posted on ${r(m)}</p>
+      <div class=${b} dangerouslySetInnerHTML=${{__html:u}}/>
+      ${s}
+      <h2 class=${h}>
+        <a target="_blank" href=${`https://github.com/zhenyanghua/zhenyanghua.github.io/blob/master/posts${c}/index.md`}>Read on GitHub</a>
+      </h2>
+    </div>`}export{l as P};
