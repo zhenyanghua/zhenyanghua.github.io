58fa41c695dbf7c73f6c7f76d0e77eff0afae932
Zhenyang Hua <edison.hua@gmail.com>
2021-09-28 09:34:09 -0400

    Updates

---
+++ chunks/writings.1d992c8b.js
@@ -0,0 +1,1 @@
+import{m as t,k as i}from"../index.e497b724.js";import"./time.daaab1ba.js";import{W as r}from"./index.c0c4f0f9.js";export default function(){return t`<${r} posts=${i} page=${{current:6,total:6,prev:"/writings/5",next:"/writings/6",last:"/writings/6",first:"/writings/1"}}/>`}
